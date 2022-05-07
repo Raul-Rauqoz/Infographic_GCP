cd170c10aae6445a3d24d6f1d3213b9ad84cb912
raul-rauqoz <raul.quinonez@telus.com>
2022-05-06 18:03:36 -0600

    feat: add mongo_on ip bridge

--- backend/mongodb/mongo_on.js
+++ backend/mongodb/mongo_on.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Connection
 const mongo_on = mongoose
 	.connect('mongodb://172.17.0.1:27017/')
-	// .connect('mongodb://host.docker.internal:27017/')
+	// .connect('mongodb://localhost:27017/')
 	.then(() => console.log('Connected to Mongo Docker'))
 	.catch((err) => console.error(err));
 
